302812cfd47ecf229483a2095ca888cb396fd6f7
Muhammad Hassan <mhassan4641@gmail.com>
2024-10-29 18:03:14 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'databinding';
+  city = 'Dera Ismail Khan';
+  num1 = 10;
+  num2 = 20;
+  changecity() {
+    this.city= 'Lahore';
+  }
+  col = 2;
+  Name = "Kashif Shah";
+  imgpath = "logo.png";
+  currentvalue = true;
+  enabaledvalue() {
+    this.currentvalue = !this.currentvalue;
+  }
+  click1()
+  {
+    console.log("Click One Fired!!");
+  }
+  click2() {
+    console.log("Click Two fired!!");
+  }
+  keyup()
+  {
+    console.log("keyup Fired!!");
+  }
+  keydown()
+  {
+    console.log("key Down Fired!!");
+  }
+  input()
+  {
+    console.log("Input Fired!!");
+  }
+  _message = "Hello world";
+  setmessage(val:any)
+  {
+    this._message = val;
+  }
+  msg = "Hello!!";
+  twoway = "Enter msg";
 }
